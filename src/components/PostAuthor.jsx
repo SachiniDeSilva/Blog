3bcb6323abbb6a4fcc2d7713e474f967825fb395
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-07-13 23:56:02 +0530

    create a author

--- src/components/PostAuthor.jsx
+++ src/components/PostAuthor.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const PostAuthor = () => {
 
-
+<Link to={'/Posts/users/sdsffv'}>
+<div className="post_author-avater">
+    <img src="" alt="" />
+</div>
+<div className="post_author-details">
+    <h5>By Ernerst</h5>
+    <small>Just Now</small>
+</div>
+</Link>
 };
 
 export default PostAuthor;
