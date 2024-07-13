88c587330a31a76863b0e06e9b4b998090323bca
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-07-13 23:19:29 +0530

    create dummy data

---
+++ src/components/Post.jsx
@@ -0,0 +1,14 @@
+
+import thumbnail1 from '../images/Serene Sunrise with Deer.jpg'
+
+const Dummy_post =[
+    {
+        id:'1',
+        thumbnail:thumbnail1,
+        category:'education',
+        title:'This is the title',
+        desc: 'djednjcnjdcnjfcnjfnjjcjcjncjcej',
+        authorID:3
+
+    },
+]
