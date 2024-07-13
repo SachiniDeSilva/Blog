03eff6f948b8a939742981c8876425d0fbfadf7f
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-07-13 23:29:59 +0530

    call to dummy data

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,9 @@
 
+
+
+import React, { useState } from 'react'
 import thumbnail1 from '../images/Serene Sunrise with Deer.jpg'
+import PostItem from './PostItem'
 
 const Dummy_post =[
     {
@@ -12,3 +16,20 @@ const Dummy_post =[
 
     },
 ]
+
+
+
+const Post = () => {
+
+    const [posts, setPosts] = useState(Dummy_post)
+  return (
+   <section className='post'>
+    {
+        posts.map((id,thumbnail, category ,title, desc, authorID) => <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} desc={desc} authorID={authorID}/>)
+    }
+
+   </section>
+  )
+}
+
+export default Post
