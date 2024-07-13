c7b0d562ff0596cb9b9ad707698bad31c200252c
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-07-13 20:05:44 +0530

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+
+    <footer>
+    <ul className='footer_categories'>
+     
+      <li><Link  to="/Posts/categories/:Agriculture">Agriculture</Link></li>
+      <li><Link  to="/Posts/categories/:Education">Education</Link></li>
+      <li><Link  to="/Posts/categories/:Business">Business</Link></li>
+      <li><Link  to="/Posts/categories/:Art">Art</Link></li>
+      <li><Link  to="/Posts/categories/:Weather">Weather</Link></li>
+      <li><Link  to="/Posts/categories/:Uncategorized">Uncatogorized</Link></li>
+
+    </ul>
+    <div className="copyright">
+      <small> All Rights Reserved &copy; Copyright</small>
+    </div>
+    </footer>
   )
 }
 
