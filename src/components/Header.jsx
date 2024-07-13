07d68439d8618054070dcb19c9886410d4ec47cf
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-07-13 18:39:45 +0530

    import root

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import logo from '../images/logo (2).png'
+import { FaBars } from 'react-icons/fa'
+import { AiOutlineClose } from 'react-icons/ai'
 const Header = () => {
   return (
-    <div>Header</div>
+   
+    <nav>
+      <div className="container nav_container">
+        <Link to="/" className='nav_logo'>
+        <img src={logo} alt='Navbar Logo'></img>
+        </Link>
+
+        <ul className="nav_menu">
+          <li><Link to='/profile'>Ernest</Link> </li>
+          <li><Link to='/create'> Create Post</Link> </li>
+          <li><Link to='/author'> Authors</Link> ernest</li>
+          <li><Link to='/logout'> Logout</Link> </li>
+        </ul>
+        <button className="nav_toggle-btn">
+<AiOutlineClose/>
+        </button>
+      </div>
+    </nav>
   )
 }
 
