25d423034a165dbda03d586e0cb0582c45b1821a
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-07-16 23:14:02 +0530

    create edit posts

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
 
 const EditPost = () => {
+
+const[title, setTitle] = useState ('')
+const [category , setCategory] =useState ('Uncategory')
+const[description, setDescription] = useState ('')
+
+const [thumbnail, setThumbnail] = useState('')
+
+
+const modules ={
+  toolbar:[
+    [{'header':[1,2,3,4,5,6 ,false]}],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [{'list': 'ordered'}, {'list': 'bulet'}, {'indent': '-1'}, {'indent': '+1'}]
+  ]
+}
+
+const formats =[
+  'header',
+  'bold', 'italic', 'underline','strtke', 'blockquote',
+  'list','builet' , 'indent',
+  'link','image'
+]
+
+const POst_categories =[
+"Agriculture", "Education", "Bussiness", "Art", "Weather", "Uncategorized"
+]
+
+
+
+
+
+
+
+
+
+
   return (
-    <div>EditPost</div>
+   <section className='create_post'>
+    <div className="container create_container">
+      <h2>Edit Post</h2>
+      <p className="form_error-message">
+        This is an error message
+      </p>
+
+      <form  className="form create-post_form">
+
+        <input type='text' placeholder='Title' value={title} onChange={e=> setTitle(e.target.value) } autoFocus/>
+
+        <select name='category' value={category} onChange={e=> setCategory(e.target.value)}>
+          {
+POst_categories.map(cat => <option key={cat}>{cat}</option>)
+          }
+          
+        </select>
+
+        <ReactQuill modules={modules} formats={formats} value={description} onChange={setDescription}/>
+          <input type='file' onChange={e => setThumbnail(e.target.files[0])} accept='png,jpg,jpeg'></input>
+          <button type='submit' className='btn primary'> Update </button>
+      </form>
+    </div>
+
+   </section>
   )
 }
 
