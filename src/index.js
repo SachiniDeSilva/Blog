93530388878bbd0321e016e25c3881961aa6f80d
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-07-17 16:25:22 +0530

    remive import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout';
 import ErrorPage from './pages/ErrorPage';
