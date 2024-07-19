62e28a41286faab008f421e8387d69410b53e43a
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-07-20 00:32:12 +0530

    not found error router

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 
 const userRoutes = require('./routes/userRoutes')
 const postRoutes = require('./routes/postRoutes')
+const {notFound, errorHandler} =require('./middleware/errorMiddleware')
 
 const app = express ();
 app.use(express.json({extended: true}))
@@ -14,4 +15,6 @@ app.use(cors({credentials:true, orgin:"http://localhost:3000"}))
 
 app.use('/api/users', userRoutes)
 app.use('/api/posts',postRoutes)
+
+app.use(notFound)
 connect(process.env.MONGO_URI).then()(app.listen(5000, ()=> console.log(`Server started on port ${process.env.PORT}`))).catch(error => {console.log(error)})
