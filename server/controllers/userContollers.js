dc2aa4a0360a5b5e88c784d0dd9f471d2b1c50f5
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-07-23 14:06:39 +0530

    change author profile picture

--- server/controllers/userContollers.js
+++ server/controllers/userContollers.js
@@ -2,9 +2,13 @@
 const bcrypt = require('bcryptjs')
 const jwt = require("jsonwebtoken")
 const User = require('../models/userModels')
+const fs = require('fs')
+const path = require('path')
+const {v4:uuid} = require("uuid")
 
 
 const HttpError = require("../models/errorModel");
+const { upload } = require('@testing-library/user-event/dist/upload')
 
 const registerUser = async (req,res,next) => {
     try{
@@ -94,9 +98,44 @@ const getUser = async(req,res,next) => {
 
 const changeAvatar = async(req,res,next) => {
   try {
-    res.json(req.files)
-    console.log(req.files)
-    
+   if(!req.files.avatar){
+    return next(new HttpError("Please choose an image ", 422))
+   }
+    //find user from database
+const user = await User.findById(req.user.id)
+
+//delete old avatar if exist 
+
+if(user.avatar){
+    fs.unlink(path.join(__dirname, '..', 'uploads', user.avatar),(err) =>{
+if(err){
+    return next(new HttpError(err))
+}
+    })
+}
+const {avatar} = req.files
+//check file size
+
+if(avatar.size > 500000){
+    return next (new HttpError("profile picture too big should be less than 500kb"),422)
+}
+let fileName 
+fileName = avatar.name
+let splittedFilename = fileName.plit('.')
+let newFilename = splittedFilename[0] +uuid() + '.' + splittedFilename[splittedFilename.length -1]
+avatar.mv(path.join(__dirname, '..', 'uploads', newFilename) ,async(err) =>{
+    if(err){
+        return next(new HttpError(err))
+    }
+
+    const updatedAvatar = await User.findByIdAndUpdate(req.user.id, {avatar:newFilename},{new:true})
+    if(!updatedAvatar){
+        return next(new HttpError("Avatar couldn't change ",422))
+    }
+    res.status(200).json(updatedAvatar)
+})
+
+
   } catch (error) {
     return next(new HttpError(error))
   }
