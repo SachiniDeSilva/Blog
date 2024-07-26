1a59ee4fb669f827c67de93dc25999c417d04cd0
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-07-26 22:45:46 +0530

    create post contollers

---
+++ server/controllers/postControllers.js
@@ -0,0 +1,42 @@
+
+//Get all posts
+const createPost = async(req, res, next) => {
+    res.json("Create post")
+}
+
+
+
+//get a new post
+const getPosts = async(req, res, next) => {
+    res.json("Get all posts post")
+}
+
+
+//Get single post
+
+const getPost = async(req, res, next) => {
+    res.json("Get single post")
+}
+
+
+
+//Get post particular catogories
+const getCatPost = async(req, res, next) => {
+    res.json("Get Catogory post")
+}
+
+//get another post
+const getUserpost = async(req, res, next) => {
+    res.json("Get user post")
+}
+
+
+//Edit post
+const editPost = async(req, res, next) => {
+    res.json("Edit post")
+}
+
+//Delete post
+const deletePost = async(req, res, next) => {
+    res.json("Delete post")
+}
