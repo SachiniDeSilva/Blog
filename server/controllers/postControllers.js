f9fd01623c65270e661f13aa81954e6ced2f7192
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-07-28 14:16:32 +0530

    do changes on post controller

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -6,7 +6,44 @@ const {v4:uuid} = require('uuid')
 const HttpError = require('../models/errorModel')
 //Get all posts
 const createPost = async(req, res, next) => {
-    res.json("Create post")
+    try {
+        let{title, category, description} = req.body
+        if(!title || !category ||!req.files ||!description
+        ){
+            return next(new HttpError("Fill in all fields and choose thambnail",422))
+        }
+
+        //check the file 
+        if(thumbnail.size > 200000){
+            return next(new HttpError("thumbnail too big. file shoud be less than 2mb."))
+
+        }
+        let fileName =thumbnail.name 
+        let splittedFilename =fileName.split('.')
+        let newFilename = splittedFilename[0] + uuid() + "." + splittedFilename[splittedFilename.length -1]
+        thumbnail.mv(path.join(__dirname, '..', '/uploads', newFilename), async(err)=>{
+            if(err){
+                return next(new HttpError(err))
+            }else{
+              const newPost = await Post.create ({title,category,description, thumbnail:newFilename, creator:req.user.id}) 
+              if(!newPost){
+                return next(new HttpError("Post couldn't not create",422))
+              } 
+              //fine user and increate post by vount1
+              const currentUser =await User.findById(req.user.id)
+              const userPostCount = current.posts + 1
+              await User.findByIdAndDelete(req.user.id,{posts:userPostCunt})
+
+
+
+
+
+              resststus(201).json(newPost)
+            }
+        })
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 
