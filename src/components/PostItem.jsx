230e1a6b75f469e81972cd1a0522760a0e75f1aa
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-08-16 00:43:15 +0530

    fix errors in post items

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom';
 import PostAuthor from './PostAuthor';
 
 const PostItem = ({ postID, category, title, desc, authorID, thumbnail, createdAt }) => {
-  const shortDescription = desc.length > 145 ? desc.substr(0, 145) + '...' : desc;
-  const postTitle = title.length > 30 ? title.substr(0, 30) + '...' : title;
+  // Add checks to ensure that 'desc' and 'title' are not undefined
+  const shortDescription = desc && desc.length > 145 ? desc.substr(0, 145) + '...' : (desc || '');
+  const postTitle = title && title.length > 30 ? title.substr(0, 30) + '...' : (title || '');
 
   return (
     <article className='post'>
@@ -16,9 +17,9 @@ const PostItem = ({ postID, category, title, desc, authorID, thumbnail, createdA
         <Link to={`/Posts/${postID}`}>
           <h3>{postTitle}</h3>
         </Link>
-        <p dangerouslySetInnerHTML={{__html:shortDescription}}/>
+        <p dangerouslySetInnerHTML={{ __html: shortDescription }} />
         <div className="post_footer">
-          <PostAuthor authorID={authorID} createdAt={createdAt}/>
+          <PostAuthor authorID={authorID} createdAt={createdAt} />
           <Link to={`/Posts/categories/${category}`} className='btn category'>{category}</Link>
         </div>
       </div>
@@ -27,4 +28,3 @@ const PostItem = ({ postID, category, title, desc, authorID, thumbnail, createdA
 };
 
 export default PostItem;
-  
